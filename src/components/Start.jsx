952880cd677ab124b6ce7c50b5ed879a2b5831d2
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-01-08 19:25:42 +0500

    Implemented pagination

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -39,8 +39,9 @@ class Start extends Component {
         this.setState({
           exam: exam,
           loading: false,
-          currentQuestion: exam.questions[this.state.providedAnswer.question],
+          currentQuestion: exam.questions[0],
         });
+        console.log(this.state.currentQuestion);
       }
     } catch (error) {
       this.setState({ error: true, loading: false });
@@ -49,15 +50,32 @@ class Start extends Component {
 
   submitQuestion = async (e) => {
     e.preventDefault();
-    const submitUrl = this.BE_URL + `/${this.state.exam._id}/answer`;
+    this.setState({ loading: true });
+    const providedAns = this.state.providedAnswer;
+    const submitUrl = this.BE_URL + `/exams/${this.state.exam._id}/answer`;
     try {
       const res = await fetch(submitUrl, {
         method: "POST",
-        body: JSON.stringify(this.state.providedAnswer),
+        body: JSON.stringify(providedAns),
+        headers: new Headers({
+          "Content-Type": "application/json",
+        }),
       });
+      console.log(res);
+      if (res.ok) {
+        providedAns.question += 1;
+        console.log(providedAns);
+        this.setState({
+          loading: false,
+          providedAnswer: providedAns,
+          currentQuestion: this.state.exam.questions[providedAns.question],
+        });
+      } else {
+        this.setState({ loading: false, error: res.statusText });
+      }
     } catch (error) {
       this.setState({
-        error: "Error occured while submitting answer. Try again",
+        error: error.text,
       });
     }
   };
@@ -74,12 +92,9 @@ class Start extends Component {
       currentQuestion,
       providedAnswer,
     } = this.state;
-    console.log("currentQuestion", currentQuestion);
     return (
       <Container>
-        {error && <Alert variant="danger">This is a alert—check it out!</Alert>}
         <Row>
-          {loading && <Spinner animation="border" variant="warning" />}
           {exam && (
             <>
               <div className="d-flex justify-content-between w-100">
@@ -89,11 +104,11 @@ class Start extends Component {
                 </h3>
               </div>
               <br />
-              <h2>Question {1}</h2>
+              <h2>Question {providedAnswer.question + 1}</h2>
 
               <p>{currentQuestion.text}</p>
               <br />
-              <form>
+              <form onSubmit={this.submitQuestion}>
                 <p>Select your answer:</p>
                 {currentQuestion.answers.map((ans, idx) => (
                   <Col key={idx}>
@@ -120,7 +135,9 @@ class Start extends Component {
               </form>
             </>
           )}
+          {loading && <Spinner animation="border" variant="warning" />}
         </Row>
+        {error && <Alert variant="danger">{error}</Alert>}
       </Container>
     );
   }
